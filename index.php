ed86f4faa716caffc1262429eb3b77127d6d04f0
sebas <sebas@Scorpion.sebas.com>
2021-02-22 13:00:53 -0300

    Final clase 1

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
 
-echo "Hey Jude!";
+// HTTP
+// ¿Qué hace el SERVIDOR HTTP?
+// 1) Recibe una petición HTTP
+$peticionRest = [
+    'url' => $_GET['url'],
+    'metodo' => $_SERVER['REQUEST_METHOD'],
+    'cuerpo' => json_decode(file_get_contents('php://input')),
+    'cabeceras' => apache_request_headers()
+];
+
+$peticionXML = [
+    'url' => $_GET['url'],
+    'metodo' => $_SERVER['REQUEST_METHOD'],
+    'cuerpo' => simplexml_load_string(file_get_contents('php://input')),
+    'cabeceras' => apache_request_headers()
+];
+
+// 2) Devuelve una respuesta HTTP
+function respuestaRest($data) {
+    header('Content-type: application/json');
+    echo json_encode($data);
+}
+
+function respuestaXML($data) {
+    header('Content-type: text/xml');
+    echo $data;
+}
+
+
+/*
+Repaso de método y cabecera
+
+*/
+
+// prfsebastianledesma@gmail.com
+
